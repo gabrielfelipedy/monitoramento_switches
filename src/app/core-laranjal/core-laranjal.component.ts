94a39296428030001ad26fe891d319ccb2892cf7
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-02-21 10:32:43 -0300

    add small table to monitor core uplinks

--- src/app/core-laranjal/core-laranjal.component.ts
+++ src/app/core-laranjal/core-laranjal.component.ts
@@ -6,16 +6,18 @@ import { ApiObservableService } from '../api-observable.service';
 import { Port } from '../../../interfaces/Port';
 import { PortList } from '../../../interfaces/PortList';
 import { Subscription } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-core-laranjal',
   standalone: true,
-  imports: [NgFor, CommonModule],
+  imports: [NgFor, CommonModule,  MatTableModule],
   templateUrl: './core-laranjal.component.html',
   styleUrl: './core-laranjal.component.scss'
 })
 export class CoreLaranjalComponent {
 
+  displayedColumns: string[] = ['name', 'inputvalue', 'outputvalue'];
   ports_core_laranjal: Array<ArrayPortElement> = new Array<ArrayPortElement>();
   private subscription!: Subscription;
 
@@ -31,7 +33,7 @@ export class CoreLaranjalComponent {
         itemid: `${i}`,
         inputvalue: data[i].lastvalue,
         outputvalue: data[i+2].lastvalue,
-        name: data[i].name,
+        name: data[i].name.slice(16, 32),
         active: data[i].lastvalue !== '0.00 KB/s' || data[i+2].lastvalue !== '0.00 KB/s'
       }
       
