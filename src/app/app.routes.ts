7ff0ecd5c792c79498a89ddd506eae0a209ad956
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-02-25 19:21:42 -0300

    add compodocs documentation on the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LaranjalSubsecaoComponent } from './laranjal-subsecao/laranjal-subsecao.component';
 import { OiapoqueSubssecaoComponent } from './oiapoque-subssecao/oiapoque-subssecao.component';
 import { HomeComponent } from './home/home.component';
+import { DocsComponent } from './docs/docs.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'laranjal', component: LaranjalSubsecaoComponent},
-    { path: 'oiapoque', component: OiapoqueSubssecaoComponent}
+    { path: 'oiapoque', component: OiapoqueSubssecaoComponent},
+    { path: 'docs', component: DocsComponent}
 ];
 
 RouterModule.forRoot(routes)
